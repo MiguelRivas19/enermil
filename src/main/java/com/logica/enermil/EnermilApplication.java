8f2ef28825209091c52def0180f10e3cc1aa1832
RIVAS ARELLANO, Miguel <miguel.rivasarellano@cgi.com>
2023-11-18 07:46:23 +0100

    initial commit

---
+++ src/main/java/com/logica/enermil/EnermilApplication.java
@@ -0,0 +1,13 @@
+package com.logica.enermil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnermilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnermilApplication.class, args);
+	}
+
+}
